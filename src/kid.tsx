ccec4d876467d2a8018dd7d92d05775c095cb0d9
Igor Rootsi <igor.rootsi@gmail.com>
2017-11-26 21:05:24 +0200

    initial commit

--- src/kid.tsx
+++ src/kid.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
 
 interface KidState {
-    count: number;
+  count: number;
 }
 class Kid extends React.Component<{}, KidState> {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            count: 0
-        };
+    this.state = {
+      count: 0
+    };
 
-        setInterval(() => {
-            this.setState({ count: this.state.count + 1 });
-        }, 1000);
-    }
-    render() {
-        return (
-            <div>
-                Kid {this.state.count}
-            </div>
-        );
-    }
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  }
+  render() {
+    return (
+      <div>
+        Kid {this.state.count}
+      </div>
+    );
+  }
 }
 
 export default Kid;
